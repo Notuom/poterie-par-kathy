973608934d3184f03636a12a76bbbd5f22e01b6a
Notuom <thibault.karl@gmail.com>
2022-12-25 20:27:10 -0500

    feat: stable json representation for identical posts

--- data/instagram/update.js
+++ data/instagram/update.js
@@ -30,27 +30,38 @@ const ALLOWED_POST_TYPES = ["IMAGE", "CAROUSEL_ALBUM"];
   );
 
   console.log("Resizing images");
-  imagePosts.forEach(async (post) => {
-    const imagePath = fileURLToPath(
-      new URL(`../../public/images/posts/${post.id}.jpg`, import.meta.url)
-    );
-    if (existsSync(imagePath)) {
-      console.log(`Image already exists for post ${post.id}`);
-      return;
-    }
-    console.log(`Resizing image for post ${post.id}`);
-    const buffer = await fetch(post.media_url).then((r) => r.arrayBuffer());
-    try {
-      await sharp(Buffer.from(buffer)).resize(400, 400).toFile(imagePath);
-    } catch (e) {
-      console.error("Error writing image to file", imagePath, e);
-    }
-  });
+  const finalPosts = await Promise.all(
+    imagePosts.map(async (post) => {
+      const thumbnailPath = `/images/posts/${post.id}.jpg`;
+      const imagePath = fileURLToPath(
+        new URL(`../../public/${thumbnailPath}`, import.meta.url)
+      );
+
+      if (existsSync(imagePath)) {
+        console.log(`Image already exists for post ${post.id}`);
+      } else {
+        console.log(`Resizing image for post ${post.id}`);
+        const buffer = await fetch(post.media_url).then((r) => r.arrayBuffer());
+        try {
+          await sharp(Buffer.from(buffer)).resize(400, 400).toFile(imagePath);
+        } catch (e) {
+          console.error("Error writing image to file", imagePath, e);
+        }
+      }
+
+      return {
+        id: post.id,
+        caption: post.caption,
+        permalink: post.permalink,
+        thumbnail: thumbnailPath,
+      };
+    })
+  );
 
   const outputPath = new URL("posts.json", import.meta.url);
   console.log(`Writing result to ${outputPath}...`);
   try {
-    writeFileSync(outputPath, JSON.stringify(imagePosts));
+    writeFileSync(outputPath, JSON.stringify(finalPosts));
   } catch (e) {
     console.error("Error writing posts file", outputPath, e);
   }
